a18fd82f8c021a6a4c6d5056c79faf8b4f44bf63
Zerooboys <121633337+Zerooboys@users.noreply.github.com>
2022-12-30 22:51:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,8 +25,8 @@ async def start(event):
     link_preview=False,
     buttons=(
       [
-        Button.url('📣 Channel', 'https://t.me/harp_tech'),
-        Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')
+        Button.url('🕊 Channel', 'https://t.me/harp_tech'),
+        Button.url('🧸 Source', 'https://github.com/AnjanaMadu/MentionAllBot')
       ]
     )
   )
@@ -39,8 +39,8 @@ async def help(event):
     link_preview=False,
     buttons=(
       [
-        Button.url('📣 Channel', 'https://t.me/harp_tech'),
-        Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')
+        Button.url('🕊 Channel', 'https://t.me/harp_tech'),
+        Button.url('🧸 Source', 'https://github.com/AnjanaMadu/MentionAllBot')
       ]
     )
   )
